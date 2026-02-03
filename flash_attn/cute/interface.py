3399ca61c2b4c2db12736f025d8f472f6f3da463
Edenzzzz <wtan45@wisc.edu>
2026-02-03 23:28:26 +0000

    add after_thread_sync and fix quant loop count

--- flash_attn/cute/interface.py
+++ flash_attn/cute/interface.py
@@ -55,6 +55,40 @@ from flash_attn.cute.block_sparsity import (
     get_block_sparse_expected_shapes,
 )
 
+def dump_kernel_attributes(compiled_kernel):
+    from cuda.bindings import driver
+    from cutlass.utils import HardwareInfo
+    import torch
+    device_id = torch.cuda.current_device()
+    hardware_info = HardwareInfo(device_id=device_id)
+    cubin_data = compiled_kernel.artifacts.CUBIN
+    if cubin_data is None:
+        print("cubin_data is None, need '--keep-cubin' option when compiling to print kernel attributes")
+        return
+    cuda_library = hardware_info._checkCudaErrors(
+        driver.cuLibraryLoadData(cubin_data, None, None, 0, None, None, 0)
+    )
+    kernels = hardware_info._checkCudaErrors(driver.cuLibraryEnumerateKernels(1, cuda_library))
+    kernel = hardware_info._checkCudaErrors(driver.cuKernelGetFunction(kernels[0]))
+    # more metrics: https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__EXEC.html#group__CUDA__EXEC_1g5e92a1b0d8d1b82cb00dcfb2de15961b
+    local_size_bytes = hardware_info._checkCudaErrors(
+        driver.cuFuncGetAttribute(
+            driver.CUfunction_attribute.CU_FUNC_ATTRIBUTE_LOCAL_SIZE_BYTES,
+            kernel,
+        )
+    )
+    num_regs = hardware_info._checkCudaErrors(
+        driver.cuFuncGetAttribute(
+            driver.CUfunction_attribute.CU_FUNC_ATTRIBUTE_NUM_REGS,
+            kernel,
+        )
+    )
+
+    print(f"--- Kernel Info ---")
+    print(f"local_size_bytes: {local_size_bytes}")
+    print(f"num_regs: {num_regs}")
+    print(f"--- End Kernel Info ---")
+
 def maybe_contiguous(x):
     return x.contiguous() if x is not None and x.stride(-1) != 1 else x
 
@@ -704,11 +738,8 @@ def _flash_attn_fwd(
             compile_args.extend([mSFQ_tensor, mSFK_tensor, mSFV_tensor])
         _flash_attn_fwd.compile_cache[compile_key] = cute.compile(*compile_args, options="--enable-tvm-ffi",
         )
-        # test 
-        if force_fp4_impl:
-            run_args = compile_args[1:]
-            run_args[6] = current_stream
-            _flash_attn_fwd.compile_cache[compile_key](*run_args)
+        # dump_kernel_attributes(_flash_attn_fwd.compile_cache[compile_key])
+
 
     # Expand block sparse tensors to match actual head count (may be broadcast from 1)
     normalized_block_sparse_tensors = None
