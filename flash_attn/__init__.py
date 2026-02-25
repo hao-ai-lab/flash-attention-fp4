c7559c0b370fec07c9b13df5aa684d45c2a38f80
Wenxuan Tan <wtan45@wisc.edu>
2026-02-25 03:30:43 +0000

    temp fix: allow running FA4 without FA2 C++ extension

--- flash_attn/__init__.py
+++ flash_attn/__init__.py
@@ -1,11 +1,14 @@
 __version__ = "2.8.3"
 
-from flash_attn.flash_attn_interface import (
-    flash_attn_func,
-    flash_attn_kvpacked_func,
-    flash_attn_qkvpacked_func,
-    flash_attn_varlen_func,
-    flash_attn_varlen_kvpacked_func,
-    flash_attn_varlen_qkvpacked_func,
-    flash_attn_with_kvcache,
-)
+try:
+    from flash_attn.flash_attn_interface import (
+        flash_attn_func,
+        flash_attn_kvpacked_func,
+        flash_attn_qkvpacked_func,
+        flash_attn_varlen_func,
+        flash_attn_varlen_kvpacked_func,
+        flash_attn_varlen_qkvpacked_func,
+        flash_attn_with_kvcache,
+    )
+except ImportError:
+    pass  # flash_attn_2_cuda (FA2 C++ ext) not built; CuTe DSL paths still work
